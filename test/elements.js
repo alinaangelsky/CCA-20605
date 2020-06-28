a6f008b652b71223c53d80ec7a14350d929a7574
Alina Archangelsky <alina@archangelsky.us>
2020-06-27 22:15:39 -0700

    6/27/2020

--- test/elements.js
+++ test/elements.js
@@ -1,34 +1,97 @@
-import assert from 'assert';
-describe('Complex Counter App', function () { //define suite title by passing a string
+import { assert } from 'chai';
+const general = require('./../data/selectors.json').general;
+const values = require('./../data/expected.json');
+    describe('Complex Counter App', function () { //define suite title by passing a string
 
-    describe('Getting to the page', function () { //define sub-suite title by passing a string
+        describe('Getting to the page', function () { //define sub-suite title by passing a string
 
-        it('TC-001 Page title is Complex Counter App', function () { //define test title by passing a string
-            browser.url('https://likejean.github.io/homework-5/'); //open baseUrl
-            const title = browser.getTitle(); //get page title and assign it to the "title" variable
-            browser.pause(2000); //just pause to visually see that something is happening on the page
-            expect(title).toEqual('Complex Counter App'); //compare {title} (actual) and "Complex Counter App" (expected)
-        })
+            it('TC-001 Page title is Complex Counter App', function () { //define test title by passing a string
+                browser.url('https://likejean.github.io/homework-5/'); //open baseUrl
+                const title = browser.getTitle(); //get page title and assign it to the "title" variable
+                browser.pause(2000); //just pause to visually see that something is happening on the page
+                expect(title).toEqual('Complex Counter App'); //compare {title} (actual) and "Complex Counter App" (expected)
+            })
 
-    });
+        });
 
-    describe('Elements exist', function () {
+        describe('Elements exist', function () {
 
-        it('TC-002 Header', function () {
-            const header = $('h1').isDisplayed();
-            expect(header).toEqual(true);
-        })
+            it('TC-002 Header', function () {
+                const header = $(general.header);
+                const actual = header.isDisplayed();
+                expect(actual).toEqual(true);
+            })
 
-        it('TC-003 Total Result', function () {
-            const header = $('h3.total-count').isDisplayed();
-            expect(header).toEqual(true);
-        })
+            it('TC-003 Total Result', function () {
+                const header = $(general.totalResult).isDisplayed();
+                expect(header).toEqual(true);
+            })
 
-        it('TC-004 Counter Name', function () {
-            const header = $$('h3')[1].isDisplayed();
-            expect(header).toEqual(true);
-        })
+            it('TC-004 Add Name Field', function () {
+                const header = $(general.addNameField).isDisplayed();
+                expect(header).toEqual(true);
+            })
 
-    });
+            it('TC-005 Label for Add Name Field', function () {
+                const actual = $$(general.addNameFieldLabel)[$$(general.addNameFieldLabel).length - 2].isDisplayed();
+                expect(actual).toEqual(true);
+            })
 
-});
+            it('TC-006 Default Value Field', function () {
+                const actual = $(general.defaultValueField).isDisplayed();
+                expect(actual).toEqual(true);
+            })
+
+            it('TC-007 Label for Default Value Field', function () {
+                const actual = $$(general.defaultValueFieldLabel)[$$(general.defaultValueFieldLabel).length - 1].isDisplayed();
+                expect(actual).toEqual(true);
+            })
+
+            it('TC-008 Add Counter', function () {
+                const actual = $(general.addCounterBtn).isDisplayed();
+                expect(actual).toEqual(true);
+            })
+
+        });
+
+        describe('Elements value', function () {
+
+            it('TC-009 Header = Counter', function () {
+                const actual = $(general.header).getText();
+                expect(actual).toEqual(values.general.header);
+
+            });
+
+            it('TC-010 Total Result = Total: 0', function () {
+                const actual = $(general.totalResult).getText();
+                expect(actual).toEqual(values.general.totalResult);
+
+            });
+
+            it('TC-011 Label for Add Name Field = Enter Counter Title:', function () {
+                const actual = $(general.addNameField).getValue();
+                expect(actual).toEqual(values.general.addNameField);
+
+            });
+
+            it('TC-012 Placeholder for Add Name Field = Counter Name', function () {
+                const actual = $$(general.addNameFieldLabel)[$$(general.addNameFieldLabel).length - 2].getText();
+                expect(actual).toEqual(values.general.addNameFieldLabel);
+            })
+
+            it('TC-013 Label for Default Value Field = Enter Initial Count:', function () {
+                const actual = $(general.defaultValueField).getValue();
+                expect(actual).toEqual(values.general.defaultValueField);
+            })
+
+            it('TC-014 Placeholder for Default Calue Field = 50 ', function () {
+                const actual = $$(general.defaultValueFieldLabel)[$$(general.defaultValueFieldLabel).length - 1].getText();
+                expect(actual).toEqual(values.general.defaultValueFieldLabel);
+            })
+
+            it('TC-015 Add Counter = ADD COUNTER', function () {
+                const actual = $(general.addCounterBtn).getText();
+                expect(actual).toEqual(values.general.addCounterBtn);
+            })
+        });
+    })
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,12 +17,12 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/*.js',
-        './test/defaultfunctionality.js'
+        './test/*.js'
     ],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
+        './test/defaultfunctionality.js'
     ],
     //
     // ============
